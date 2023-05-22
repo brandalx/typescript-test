55f6d726732eec3d543ab50d7880c97b01342d7d
brndalx <brndalx@gmail.com>
2023-05-22 03:17:51 +0300

    feat: initial commit chekin types

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//basic types
+let id = 5;
+console.log("ID:", id);
+let company = "Traversy media";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+// ids.push("hello") //
+ids.push(5);
+//tuple
+let person = [1, "Brad", true];
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//basic types
+let id: number = 5;
+console.log("ID:", id);
+let company: string = "Traversy media";
+let isPublished: boolean = true;
+let x: any = "Hello";
+let ids: number[] = [1, 2, 3, 4, 5];
+// ids.push("hello") //
+ids.push(5);
+
+//tuple
+let person: [number, string, boolean] = [1, "Brad", true];
